7ea4b7b6bc152baec5a32e30bd8624318ef2af09
Manuel Blanco <manuel.blanco77@gmail.com>
2020-08-15 16:10:56 -0300

    Subiendo primeras pruebas con Enzyme, snapshots con createSerializer

--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,20 +1,22 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+//import { render } from '@testing-library/react';
 import PrimeraApp from '../PrimeraApp';
 import React from 'react';
+import { shallow } from 'enzyme';
 
 
 describe('Pruebas en <PrimeraApp />', () => {
-    
+    // test('Demostrar el mensaje "Hola soy goku"', () => {
+    //     const saludo = 'Hola soy goku';
+    //     const { getByText } = render( <PrimeraApp saludo={ saludo }/> )
+    //     expect( getByText( saludo ) ).toBeInTheDocument();
+    // })
 
-    test('Demostrar el mensaje "Hola soy goku"', ()=>{
+    test('debe de mostrar <PrimeraApp/> correctamente', ()=>{
 
-        const saludo = 'Hola soy goku';
-        const { getByText } = render( <PrimeraApp saludo={ saludo }/> )
-       
-        expect( getByText( saludo ) ).toBeInTheDocument();
-        
+        const wrapper = shallow( <PrimeraApp saludo='Hola soy goku'/> );
+
+        expect( wrapper ).toMatchSnapshot();
     })
-
 
 })
