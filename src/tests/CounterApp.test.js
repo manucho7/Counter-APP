b09c461db67098ee1290dbf37055ec345bea6b48
Manuel Blanco <manuel.blanco77@gmail.com>
2020-08-16 13:24:33 -0300

    Subiendo pruebas en <CounterApp/>

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -6,20 +6,46 @@ import '@testing-library/jest-dom';
 import React from 'react';
 
 describe('Pruebas en el <CounterApp/>', () => {
+    let wrapper = shallow( <CounterApp /> );
+
+    beforeEach( () => {
+
+        wrapper = shallow( <CounterApp /> );
+    })
     
     test('debe mostrar <CounterApp/> correctamente', () => {
 
-        const wrapper = shallow( <CounterApp />)
         expect( wrapper ).toMatchSnapshot();
 
     })
     
-test('debe de mostrar el valor por defecto de 100', () => {
+    test('debe de mostrar el valor por defecto de 100', () => {
 
-    const wrapper = shallow( <CounterApp value={ 100 } />);
+        const wrapper = shallow( <CounterApp value={ 100 } />);
+        
+        const counterText = wrapper.find('h2').text();
 
-    const counterText = wrapper.find('h2').text();
-    expect( counterText ).toBe( '100' );
-})
-    
+        expect( counterText ).toBe( '100' );
+    })
+
+
+    test('debe de incrementar con el boton +1', () => {
+        //busco el boton de la posicion 0 - el primero 
+        wrapper.find('button').at(0).simulate('click');
+        
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect( counterText ).toBe('11');
+    })
+
+
+    test('debe de decrementar con el boton -1 ', () => {
+        //busco el boton de la posicion 2 el ultimo
+        wrapper.find('button').at(2).simulate('click');
+        
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect( counterText ).toBe('9');
+    })
+
 })
