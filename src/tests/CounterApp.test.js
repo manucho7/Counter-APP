a9091cf234d4fdf623aee19c2adde6c14b51ab55
Manuel Blanco <manuel.blanco77@gmail.com>
2020-08-16 13:38:06 -0300

    Subiendo pruebas en <CounterApp/> con Enzyme

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -17,7 +17,7 @@ describe('Pruebas en el <CounterApp/>', () => {
 
         expect( wrapper ).toMatchSnapshot();
 
-    })
+    });
     
     test('debe de mostrar el valor por defecto de 100', () => {
 
@@ -26,7 +26,7 @@ describe('Pruebas en el <CounterApp/>', () => {
         const counterText = wrapper.find('h2').text();
 
         expect( counterText ).toBe( '100' );
-    })
+    });
 
 
     test('debe de incrementar con el boton +1', () => {
@@ -36,7 +36,7 @@ describe('Pruebas en el <CounterApp/>', () => {
         const counterText = wrapper.find('h2').text().trim();
 
         expect( counterText ).toBe('11');
-    })
+    });
 
 
     test('debe de decrementar con el boton -1 ', () => {
@@ -46,6 +46,21 @@ describe('Pruebas en el <CounterApp/>', () => {
         const counterText = wrapper.find('h2').text().trim();
 
         expect( counterText ).toBe('9');
-    })
+    });
+
+
+    test('debe de colocar el valor por defecto con el btn reset', () => {
+        
+        const wrapper = shallow( <CounterApp value={ 105 } />);
+
+        wrapper.find( 'button' ).at(0).simulate( 'click' );
+        wrapper.find( 'button' ).at(0).simulate( 'click' );
+        wrapper.find( 'button' ).at(1).simulate( 'click' );
+
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect( counterText ).toBe( '105' );
+    });
+    
 
 })
